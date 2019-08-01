324b9f716bc819e9809347a7950f3b3f0112d51c
dreadlockdrew <Drewsulker@gmail.com>
2019-08-01 00:33:16 -0400

    Itemize Chunk testing Methods in TestField Class and refractor method names for ease of reading.

--- src/model/TestField.scala
+++ src/model/TestField.scala
@@ -2,19 +2,37 @@ package model
 
 object TestField {
 
+  def ProtoGenerateContents(chunk: Chunk): Unit ={
+    for(row <- 0 until chunk.chunkSize ; column <- 0 until chunk.chunkSize)
+    {
+      if(row==0 || row == chunk.chunkSize-1 || column==0 || column==chunk.chunkSize-1)
+      {chunk.contents(row)(column) = new UnobtainiumWall}
+      else
+      {chunk.contents(row)(column) = new UnobtainiumPath}
+    }
+  }
+
+  def ProtoPrintChunkContents(specification:String,chunk: Chunk): Unit =
+  {
+   if ( specification == "name")
+     {    for(i <- 0 until chunk.chunkSize)
+     {
+       for(j <-0 until chunk.chunkSize)
+       {
+         print( chunk.contents(i)(j).name + " | ")
+       }
+       println("")
+     }}
+
+  }
+
   def main(args: Array[String]): Unit = {
     val testChunk = new Chunk(0,0,10)
-    testChunk.GenerateContents
+
+    ProtoGenerateContents(testChunk)
+    ProtoPrintChunkContents("name",testChunk)
 
 
-    for(i <- 0 until testChunk.chunkSize)
-      {
-        for(j <-0 until testChunk.chunkSize)
-          {
-            print( testChunk.contents(i)(j).name + " | ")
-          }
-        println("")
-      }
 
 
   }
