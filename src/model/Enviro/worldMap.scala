118f820baf2606f62a9fb717cb9e9fe7609365ee
dreadlockdrew <drewsulk@buffalo.edu>
2021-08-19 15:05:42 -0500

    Fixed an issue where chunk loader would only load as many chunks as the maximum chunk size instead of the maximum chunk count.
    Fixed chunk lookup function so that it returned proper cords when given bad input

--- src/model/Enviro/worldMap.scala
+++ src/model/Enviro/worldMap.scala
@@ -17,9 +17,9 @@ class worldMap(val chunkSize:Int, val mapSize: Int)
       return Json.toJson(outPut)
 
     }
-  var chunkLoaders:Array[(Int,Int)]=Array.empty
-  val orthoChunkCount = mapSize/chunkSize //The Total Number of chunks a world should contain
-  var chunks:Array[Array[Chunk]] = Array.ofDim(chunkSize,chunkSize)//The currently loaded chunks
+  var chunkLoaders:Array[(Int,Int)]=Array.empty//List of chunks that are actually loaded at any given time.
+  val orthoChunkCount = mapSize/chunkSize //The Total Number of chunks a world should contain loaded or otherwise
+  var chunks:Array[Array[Chunk]] = Array.ofDim(orthoChunkCount,orthoChunkCount)//The currently loaded chunks
   if (chunkSize <= 4) {throw new RuntimeException("Chunk size too small")}
   if (mapSize < chunkSize) {throw new RuntimeException("Chunk size cannot be smaller then mapSize")}
   if (mapSize % chunkSize != 0) {throw new RuntimeException("mapSize must be divisible by chunkSize")}
@@ -63,17 +63,35 @@ class worldMap(val chunkSize:Int, val mapSize: Int)
     {chunks(x)(y).GenerateContents}
   }
 
-  def locateChunk(x:Int,y:Int):(Int, Int)={(x/chunkSize,y/chunkSize)}
-  /*returns a 2Tuple of the origin of the chunk were the x and y would be located
+  def alterChunk(cords:(Int,Int),Command:String):Unit={}
+  def locateChunk(cords:(Int,Int)):(Int, Int)=
+  {val  x:Float =cords._1;val y:Float =cords._2
+
+    print("searching for a chunk for "+x+" "+y+"\n")
+    val xOrig=x /chunkSize;
+    val yOrig=y /chunkSize
+    //return this normally (x/chunkSize,y/chunkSize)
+    for(i <- 0 until orthoChunkCount ; j <- 0 until orthoChunkCount)
+    {
+      var currentChunk=chunks(i)(j)
+      if(currentChunk.xloc==xOrig && currentChunk.yloc==yOrig)
+        {print("found viable chunk at "+xOrig+" "+yOrig)
+          return(currentChunk.xloc,currentChunk.yloc)}
+
+    }
+    print("OUT OF BOUNDS!!!")
+    return (null)
+  }
+    /*returns a 2Tuple of the origin of the chunk were the x and y would be located
  regardless of if the chunk physically exists or not
    */
   def loadChunkFrom_PS(originCords:(Int,Int),filePath:String):String={""}
   def saveChunkTo_PS(chunk: Chunk,filePath:String):Unit={}
-
-  def alterChunk(cords:(Int,Int),Command:String):Unit={}
   def updateChunkLoaders():Unit={}
   def addChunkLoader(cords:(Int,Int)):Unit={}//add the chunk
   def removeChunkLoader(cords:(Int,Int))={}
+
+
 
 
 }
--- src/model/Main.scala
+++ src/model/Main.scala
@@ -6,13 +6,14 @@ object Main {
   def main(args: Array[String]):Unit = {
   {
     println("Starting...")
-    var testingCords:(Int,Int) = (5,5)
+    var testingCords:(Int,Int) = (98,98)
 
-    val worldMap = new worldMap(10,100)
+    val worldMap = new worldMap(10,1000)
     worldMap.GenerateChunks
     //worldMap.populateChunks
     //print(worldMap.chunks(0)(0).gameState())
-    print(worldMap.gameState)
+    //print(worldMap.gameState)
+    worldMap.locateChunk(testingCords)
   }
   }
   def yieldInstructions(FileName:String):Unit={}
